9ef3e27c0dc3a32bd9f01d9bcf08331fb9d7c253
Gustavo Gaspar <gustavominghini@gmail.com>
2025-10-30 12:05:32 -0300

    add tests, books services and repository
    Adiciona serviços e repositórios de livros
    
    - Adicionados novos projetos ao `LibraryApi.sln`, incluindo `LibraryApiTest`, com configurações para Debug e Release.
    - Criada a interface `IBookService` para definir serviços de livros.
    - Implementada a classe `BookService` com método `AlugarLivro` para gerenciar aluguéis de livros.
    - Criado o controlador `BookController`.
    - Adicionada a propriedade `DbSet<Book> Books` no `LibraryDbContext`.
    - Criada a interface `IBookRepository` para operações CRUD de livros.
    - Implementada a classe `BookRepository` para manipulação de dados de livros.
    - Criada a classe `Book` com propriedades essenciais.
    - Adicionada a propriedade `BookId` na classe `User` para associar livros aos usuários.

---
+++ LibraryApi/Infrastructure/Repository/BookRepository.cs
@@ -0,0 +1,52 @@
+﻿using Application.Interfaces;
+using LibraryApi.Data;
+using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly LibraryDbContext _dbContext;
+
+        public BookRepository(LibraryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _dbContext.Set<Book>().ToListAsync();
+        }
+
+        public async Task<Book?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Book>().FindAsync(id);
+        }
+
+        public async Task<Book> CreateAsync(Book book)
+        {
+            var created = await _dbContext.Set<Book>().AddAsync(book);
+            await _dbContext.SaveChangesAsync();
+            return created.Entity;
+        }
+
+        public async Task<Book> UpdateAsync(Book book)
+        {
+            _dbContext.Set<Book>().Update(book);
+            await _dbContext.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var book = await _dbContext.Set<Book>().FindAsync(id);
+            if (book == null) return false;
+            _dbContext.Set<Book>().Remove(book);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
